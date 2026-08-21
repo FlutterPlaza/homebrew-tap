78b718545208cb73870dc51e5322b816252b5f0a
fonkamloic <fonkamloic@gmail.com>
2026-08-20 22:03:01 -0400

    actionforge 1.4.5

--- Formula/actionforge.rb
+++ Formula/actionforge.rb
@@ -1,16 +1,17 @@
 class Actionforge < Formula
   desc "One-click self-hosted GitHub Actions CI runners"
   homepage "https://github.com/FlutterPlaza/actionforge"
-  url "https://github.com/FlutterPlaza/actionforge/archive/refs/tags/v1.4.4.tar.gz"
-  sha256 "005ac15811beca7e0e9f6d004f575eb21e2121865ad9a0dba7c7a3aaca3a7b97"
+  url "https://github.com/FlutterPlaza/actionforge/archive/refs/tags/v1.4.5.tar.gz"
+  sha256 "55225d8795b7fe274b26609cd6bb4916f273b1e5fc93c562f84bcc4cdeb5a2ae"
   license "BSD-3-Clause"
 
   depends_on "jq"
 
   def install
-    libexec.install "setup.sh", "Dockerfile", "docker-compose.yml", "entrypoint.sh"
+    libexec.install "setup.sh", "Dockerfile", "docker-compose.yml", "entrypoint.sh", "autoscale.sh"
     chmod 0755, libexec/"setup.sh"
     chmod 0755, libexec/"entrypoint.sh"
+    chmod 0755, libexec/"autoscale.sh"
 
     (bin/"actionforge").write <<~SH
       #!/usr/bin/env bash
